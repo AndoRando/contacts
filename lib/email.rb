d148539949f2e6a3f13a28e5d24d7e96b413a691
Andrew Brennwald & Vaughn Dice <student@epicodus.com>
2015-08-13 11:06:12 -0700

    Implement save, clear, and call all methods for email class.

--- lib/email.rb
+++ lib/email.rb
@@ -1,9 +1,22 @@
 class Email
   attr_reader(:address, :type)
+  @@Emails = []
 
   def initialize (attributes)
     @address = attributes.fetch(:address)
     @type = attributes.fetch(:type)
+  end
+
+  define_singleton_method(:all) do
+    @@Emails
+  end
+
+  define_singleton_method(:clear) do
+    @@Emails.clear()
+  end
+
+  def save
+    @@Emails.push(self)
   end
 
 end
--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -18,4 +18,27 @@ describe(Email) do
       expect(@andrews_email.type()).to(eq('work'))
     end
   end
+
+  describe(".all") do
+    it('starts out empty') do
+      expect(Email.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it('returns all email addresses') do
+      @vaughns_email.save()
+      @andrews_email.save()
+      expect(Email.all()).to(eq([@vaughns_email, @andrews_email]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears all email addresses') do
+      @vaughns_email.save()
+      @andrews_email.save()
+      Email.clear()
+      expect(Email.all()).to(eq([]))
+    end
+  end
 end
